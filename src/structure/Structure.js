490b29fedb17558836a159c4f66c9a4a411cad9c
cerciber <ceatorresar>
2020-04-11 12:44:12 -0500

    Creación diseño de barra de opciones

--- src/structure/Structure.js
+++ src/structure/Structure.js
@@ -14,47 +14,74 @@ export const FIND_CODE_SCENE = 5;
 export const TANK_ATTACT_SCENE = 6;
 
 class Structure extends React.Component {
-  
+
   constructor(props) {
     super(props);
-    this.state = {scene: ABOUT_ME_SCENE}
+    this.state = { scene: ABOUT_ME_SCENE }
   }
 
   changeScene(val) {
-    this.setState({scene: val});
+    this.setState({ scene: val });
+  }
+
+  getScene() {
+    switch (this.state.scene) {
+      case ABOUT_ME_SCENE:
+        return <div>0</div>;
+      case PORTFOLIO_SCENE:
+        return <div>1</div>;
+      case CONTACT_SCENE:
+        return <div>2</div>;
+      case TRIM_CODE_SCENE:
+        return <div>3</div>;
+      case QUICK_LIST_SCENE:
+        return <div>4</div>;
+      case FIND_CODE_SCENE:
+        return <div>5</div>;
+      case TANK_ATTACT_SCENE:
+        return <div>6</div>;
+    }
   }
 
   render() {
     return (
-      <div>
+      <div class="body">
         <div class="header">
           <div class="icons">
-            Iconos
+            <img class="icon" src={process.env.PUBLIC_URL + '/Images/facebook.png'} alt="facebook" />
+            <img class="icon" src={process.env.PUBLIC_URL + '/Images/instagram.png'} alt="instagram" />
+            <img class="icon" src={process.env.PUBLIC_URL + '/Images/twitter.png'} alt="twitter" />
           </div>
           <div class="user">
             <div class="photo">
-              Photo
+              <img class="profilePhoto" src={process.env.PUBLIC_URL + '/Images/Photo.jpg'} alt="Photo" />
             </div>
             <div class="name">
-              Cesar Augusto <br/> Torres Ardila
+              Cesar Augusto <br /> Torres Ardila
 					  </div>
           </div>
           <div class="title">
             Cerciber
-				  </div>
-        </div>
-        <div class="nav">
-          <button class="button" type="button" onClick={() => this.changeScene(ABOUT_ME_SCENE)}>Acerca de mi</button>
-          <button class="button" type="button" onClick={() => this.changeScene(PORTFOLIO_SCENE)}>Portafolio</button>
-          <button class="button" type="button" onClick={() => this.changeScene(CONTACT_SCENE)}>Contacto</button>
-          <button class="button" type="button" onClick={() => this.changeScene(TRIM_CODE_SCENE)}>TrimCode</button>
-          <button class="button" type="button" onClick={() => this.changeScene(QUICK_LIST_SCENE)}>QuickList</button>
-          <button class="button" type="button" onClick={() => this.changeScene(FIND_CODE_SCENE)}>FindCode</button>
-          <button class="button" type="button" onClick={() => this.changeScene(TANK_ATTACT_SCENE)}>TankAttack</button>
-        </div>
-        <div class="content">
-          {this.state.scene}
           </div>
+        </div>
+        <nav class="tabs">
+          <ul>
+            <li class="tab" onClick={() => this.changeScene(ABOUT_ME_SCENE)}>Acerca de mí</li>
+            <div class="tab"> 
+              <li onClick={() => this.changeScene(PORTFOLIO_SCENE)}>Portafolio</li>
+              <ul class="subtabs">
+                <li class="subtab" onClick={() => this.changeScene(TRIM_CODE_SCENE)}>TrimCode</li>
+                <li class="subtab" onClick={() => this.changeScene(QUICK_LIST_SCENE)}>QuickList</li>
+                <li class="subtab" onClick={() => this.changeScene(FIND_CODE_SCENE)}>FindCode</li>
+                <li class="subtab" onClick={() => this.changeScene(TANK_ATTACT_SCENE)}>TankAttack</li>
+              </ul>
+            </div>
+            <li class="tab" onClick={() => this.changeScene(CONTACT_SCENE)}>Contacto</li> 
+          </ul>
+        </nav>
+        <div class="content">
+          {this.getScene()}
+        </div>
       </div>
     );
   }
