9ec3f7a1ffaf493e18888278e19aaeb73dcb85e4
cerciber <ceatorresar>
2020-04-13 14:20:25 -0500

    Updates

---
+++ precache-manifest.b721d284679b71acac79f9f462d587dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2965d3c23e85d51da4267b0aa722c4fa",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "42a697aa3c576e2d1f01",
+    "url": "/Portfolio/static/css/main.0cea2e0b.chunk.css"
+  },
+  {
+    "revision": "ea7752ac4d2ef1856a8d",
+    "url": "/Portfolio/static/js/2.7271ffb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.7271ffb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a697aa3c576e2d1f01",
+    "url": "/Portfolio/static/js/main.4e9eff50.chunk.js"
+  },
+  {
+    "revision": "bbf9f513ca94ccde67a9",
+    "url": "/Portfolio/static/js/runtime-main.88d4256f.js"
+  }
+]);
